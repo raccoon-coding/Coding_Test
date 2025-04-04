c6cbddb0f0a95b374a6f5704ba196005230ea8f1
raccon <minsungt@naver.com>
2025-04-04 15:16:07 +0900

    solve Dynamic Programming and in BackJoon

--- main.cpp
+++ main.cpp
@@ -7,23 +7,5 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int n;
-    cin >> n;
-
-    if(n % 2 != 0) {
-        return 0;
-    }
-
-    vector<int> dp(n + 1, 0);
-    dp[2] = 3;
-    dp[4] = 11; // dp[2] * 3 + 2
-    // dp[6] = dp[4] * 3 + dp[2] * 2;
-
-    for(int i = 3; i <= n/2; i++) {
-        dp[i * 2] = dp[(i - 1) * 2] * 3 + dp[(i - 2) * 2] * 2;
-    }
-
-    cout << dp[n] << endl;
-
     return 0;
 }
